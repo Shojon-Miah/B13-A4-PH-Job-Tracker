cfb977ea2cf9d5685e84ab8e9cccd0324fc4ff30
Shojon-Miah <sujonv21@gmail.com>
2026-02-22 19:35:51 +0600

    Jobs information section

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Jobs information section 
+
+
+const jobsData = [
+    {
+        id: 1,
+        companyName: "Mobile First Corp",
+        position: "React Native Developer",
+        location: "Remote",
+        type: "Full-time",
+        salary: "$130,000 - $ 175,000",
+        description: "Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.",
+        status: "all",
+    }
+];
+console.log(jobsData);
+
